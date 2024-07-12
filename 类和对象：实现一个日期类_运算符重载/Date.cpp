e09a38ebd923b7f2482366a7b310a67df43a541b
勤奋的土豆 <820806231@qq.com>
2024-07-12 21:13:56 +0800

    类和对象 运算符重载，实现日期类

--- 类和对象：实现一个日期类_运算符重载/Date.cpp
+++ 类和对象：实现一个日期类_运算符重载/Date.cpp
@@ -15,15 +15,57 @@ void Date::Print()
 
 bool Date::operator<(const Date& d)
 {
-
+	if (_year < d._year)
+	{
+		return true;
+	}
+	else if (_year == d._year)
+	{
+		if (_month < d._month)
+		{
+			return true;
+		}
+		else if (_month == d._month)
+		{
+			return _day < d._day;
+		}
+	}
+	return false; // 明确返回false
 }
 
 
-bool operator<=(const Date& d);
-bool operator>(const Date& d);
-bool operator>=(const Date& d);
-bool operator==(const Date& d);
-bool operator!=(const Date& d);
+bool Date::operator<=(const Date& d)
+{
+		return *this < d 
+			|| *this == d;
+}
+
+bool Date::operator>(const Date& d)
+{
+	return !(*this <= d);
+}
+
+bool Date::operator>=(const Date& d)
+{
+	return *this > d
+		&& *this == d;
+}
+
+bool Date::operator==(const Date& d)
+{
+	return _year != d._year
+		&& _month != d._month
+		&& _day != d._day;
+}
+
+
+bool Date::operator!=(const Date& d)
+{
+	return _year != d._year
+		|| _month != d._month
+		|| _day != d._day;
+}
+
 
 Date& Date::operator+=(int day)
 {
@@ -43,7 +85,35 @@ Date& Date::operator+=(int day)
 	return *this;
 }
 
+// this不改变  
+Date Date::operator-(int day)
+{
+	_day -= day;
+	if (_day <= 0)
+	{
+		while (_day < 1)
+		{
+			if (_month == 1)
+			{
+				_month = 12;
+				_year--;
+			}
+			else
+			{
+				_month--;
+			}
+			_day += GetMonthDay(_year, _month); // 这里需要一个函数来获取每个月的天数
+		}
+	}
+	return *this; // 返回对象本身的引用
+}
 
-Date& operator+(int day);
 
-Date operator-(int day);
+Date Date::operator+(int day)
+{
+	Date tmp = *this;
+
+	tmp += day;
+
+	return tmp;
+}
--- 类和对象：实现一个日期类_运算符重载/Test.cpp
+++ 类和对象：实现一个日期类_运算符重载/Test.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
+	Date d1(2024, 7, 12);
+	Date d2 = d1 + 100;
+	//Date d3(d1 + 100);
+	d1.Print();
+	d2.Print();
 
+	//d1 += 100;
+	//d1.Print();
+
+	d1 += 30000;
+	d1.Print();
 
 	return 0;
 }

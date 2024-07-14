4391a26de49716b94519b9b61bce7e0eb7d8cde5
勤奋的土豆 <820806231@qq.com>
2024-07-14 23:42:49 +0800

    类和对象复习

---
+++ 类和对象下：初始化列表，static，友元，内部类/Datea.cpp
@@ -0,0 +1,68 @@
+﻿//class A
+//{
+//public :
+//	A()
+//	{
+//		++_scount;
+//	} 
+//
+//	A(const A& t)
+//	{
+//		++_scount;
+//	} 
+//	
+//	~A()
+//	{
+//		--_scount;
+//	}
+//
+//	static int GetACount()
+//	{
+//		return _scount;
+//	}
+//
+//private:
+//	// 类⾥⾯声明
+//	static int _scount;
+//};
+//// 类外⾯初始化
+//i
+// 
+// 
+// nt A::_scount = 0;
+
+
+
+
+//class A
+//{ 
+//private:
+//	static int _k;
+//	int _h = 1;
+//
+//public:
+//	class B // B默认就是A的友元
+//	{ 
+//		public:
+//			void foo(const A& a)
+//			{
+//				cout << _k << endl; //OK
+//				cout << a._h << endl; //OK
+//			}
+//	};
+//};
+//int A::_k = 1;
+//
+//int main()
+//{
+//	cout << sizeof(A) << endl;
+//
+//	A::B b;
+//
+//	A aa;
+//	b.foo(aa);
+//
+//	return 0;
+//}
+
+//<int>

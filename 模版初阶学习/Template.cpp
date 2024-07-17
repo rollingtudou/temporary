a0d6521a81c5d516fb20e4a386a751616c97e4b4
勤奋的土豆 <820806231@qq.com>
2024-07-17 23:31:40 +0800

    模版学习

---
+++ 模版初阶学习/Template.cpp
@@ -0,0 +1,49 @@
+//void Swap(int& left, int& right)
+//{
+//	int temp = left;
+//	left = right;
+//	right = temp;
+//} 
+//
+//void Swap(double& left, double& right)
+//{
+//	double temp = left;
+//	left = right;
+//	right = temp;
+//} 
+//
+//void Swap(char& left, char& right)
+//{
+//	char temp = left;
+//	left = right;
+//	right = temp;
+//}
+
+//template<typename T>
+//void Swap(T& left, T& right)
+//{
+//	T temp = left;
+//	left = right;
+//	right = temp;
+//}
+
+
+//T Add(const T& left, const T& right)
+//{
+//	return left + right;
+//}
+
+
+
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//	return left + right;
+//} 
+//
+//// 通用加法函数
+//template<class T1, class T2>
+//T1 Add(T1 left, T2 right)
+//{
+//	return left + right;
+//}

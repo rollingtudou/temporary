bd4a1bb7f77bc0fa7c787f8e4aab399a03052202
勤奋的土豆 <820806231@qq.com>
2024-07-20 22:54:04 +0800

    复习

--- Holiday_bite_work/Test.cpp
+++ Holiday_bite_work/Test.cpp
@@ -38,3 +38,78 @@
 //    aa.Print();
 //
 //}
+
+
+
+//#include<iostream>
+//#include<cstdio>
+//using namespace std;
+//int DayTab[2][13] = {
+//    {0,31,28,31,30,31,30,31,31,30,31,30,31},
+//    {0,31,29,31,30,31,30,31,31,30,31,30,31},
+//};
+//bool IsLeapYear(int year) {
+//    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+//}
+//int YearTab[2] = { 365,366 };
+//int main() {
+//    int year1, month1, day1;
+//    int year2, month2, day2, minyear, maxyear;
+//    int number, num1, num2;
+//    while (scanf("%04d%02d%02d\n%04d%02d%02d", &year1, &month1, &day1, &year2, &month2, &day2) != EOF) 
+//    {
+//        number = 0;
+//        minyear = year1 < year2 ? year1 : year2;
+//        maxyear = (minyear == year1) ? year2 : year1;
+//        while (minyear < maxyear) {
+//            number += YearTab[IsLeapYear(minyear)];
+//            ++minyear;
+//        }
+//        num1 = 0;
+//        for (int i = 0; i < month1; ++i) {
+//            num1 += DayTab[IsLeapYear(year1)][i];
+//        }
+//        num1 += day1;
+//        num2 = 0;
+//        for (int i = 0; i < month2; ++i) {
+//            num2 += DayTab[IsLeapYear(year2)][i];
+//        }
+//        num2 += day2;
+//        number += (year1 < year2 ? (num2 - num1 + 1) : (num1 - num2 + 1));
+//        cout << number << endl;
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 类和对象：实现一个日期类_运算符重载/Date.h
+++ 类和对象：实现一个日期类_运算符重载/Date.h
@@ -1,53 +1,119 @@
-#pragma once
+//#pragma once
+//
+//#include<iostream>
+//using namespace std;
+//#include<assert.h>
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1);
+//
+//	void Print();
+//
+//
+//	// 得出在当前年份和月份下的当月天数是多少
+//	int GetMonthDay(int year,int month)
+//	{
+//		static int monthDayArray[13] = { -1, 31, 28, 31, 30, 31, 30,
+//							31, 31, 30, 31, 30, 31 };
+//
+//		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+//		{
+//			return 29;
+//		}
+//
+//		return monthDayArray[month];
+//	}
+//
+//	bool operator<(const Date& d);
+//	bool operator<=(const Date& d);
+//	bool operator>(const Date& d);
+//	bool operator>=(const Date& d);
+//	bool operator==(const Date& d);
+//	bool operator!=(const Date& d);
+//
+//	Date operator+(int day);
+//	Date& operator+=(int day);
+//
+//	Date operator-(int day);
+//
+//	bool CheckDate();
+//
+//	Date operator++(int);
+//	Date& operator++();
+//
+//	friend ostream& operator<<(ostream& out, const Date& d);
+//	friend istream& operator>>(istream& in, Date& d);
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//
+//};
 
-#include<iostream>
-using namespace std;
-#include<assert.h>
 
-class Date
-{
-public:
-	Date(int year = 1900, int month = 1, int day = 1);
-
-	void Print();
-
-
-	// 得出在当前年份和月份下的当月天数是多少
-	int GetMonthDay(int year,int month)
-	{
-		static int monthDayArray[13] = { -1, 31, 28, 31, 30, 31, 30,
-							31, 31, 30, 31, 30, 31 };
-
-		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
-		{
-			return 29;
-		}
-
-		return monthDayArray[month];
-	}
-
-	bool operator<(const Date& d);
-	bool operator<=(const Date& d);
-	bool operator>(const Date& d);
-	bool operator>=(const Date& d);
-	bool operator==(const Date& d);
-	bool operator!=(const Date& d);
-
-	Date operator+(int day);
-	Date& operator+=(int day);
-
-	Date operator-(int day);
-
-	bool CheckDate();
-
-	Date operator++(int);
-	Date& operator++();
-
-	friend ostream& operator<<(ostream& out, const Date& d);
-	friend istream& operator>>(istream& in, Date& d);
-private:
-	int _year;
-	int _month;
-	int _day;
-
-};
+//
+//int globalVar = 1;
+//
+//static int staticGlobalVar = 1;
+//
+//void Test()
+//
+//{
+//
+//	static int staticVar = 1;
+//
+//	int localVar = 1;
+//
+//
+//
+//	int num1[10] = { 1, 2, 3, 4 };
+//
+//	char char2[] = "abcd";
+//
+//	char* pChar3 = "abcd";
+//
+//	int* ptr1 = (int*)malloc(sizeof(int) * 4);
+//
+//	int* ptr2 = (int*)calloc(4, sizeof(int));
+//
+//	int* ptr3 = (int*)realloc(ptr2, sizeof(int) * 4);
+//
+//	free(ptr1);
+//
+//	free(ptr3);
+//
+//}
+//
+//
+//
+//1. 选择题：
+//
+//选项 : A.栈 B.堆 C.数据段(静态区) D.代码段(常量区)
+//
+//globalVar在哪里？____  staticGlobalVar在哪里？____
+//
+//staticVar在哪里？____  localVar在哪里？____
+//
+//num1 在哪里？____
+//
+//
+//
+//char2在哪里？____ * char2在哪里？___
+//
+//pChar3在哪里？____ * pChar3在哪里？____
+//
+//ptr1在哪里？____ * ptr1在哪里？____
+//
+//
+//
+//2. 填空题：
+//
+//sizeof(num1) = ____;
+//
+//sizeof(char2) = ____;   strlen(char2) = ____;
+//
+//sizeof(pChar3) = ____;   strlen(pChar3) = ____;
+//
+//sizeof(ptr1) = ____;       

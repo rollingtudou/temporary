2c864705518f7d2ffc46b3e0065b029d913a211f
勤奋的土豆 <820806231@qq.com>
2024-07-22 14:09:32 +0800

    课后作业

--- Holiday_bite_work/Test.cpp
+++ Holiday_bite_work/Test.cpp
@@ -85,30 +85,212 @@
 
 
 
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//
+//	return 0;
+//}
+//
 
-#include<iostream>
-using namespace std;
+//
+//
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//
+//	return 0;
+//}
+//
 
-int main()
+
+//﻿int main(int argc, char* argv[])
+//
+//{
+//
+//	string a = "hello world";
+//
+//	string b = a;
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	string c = b;
+//
+//	c = "";
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	a = "";
+//
+//	if (a.c_str() == b.c_str())
+//
+//	{
+//
+//		cout << "true" << endl;
+//
+//	}
+//
+//	else cout << "false" << endl;
+//
+//	return 0;
+//
+//}
+//
+//
+
+
+/*
+
+﻿int main(int argc, char* argv[])
+
 {
 
+	string strText = "How are you?";
+
+	string strSeparator = " ";
+
+	string strResult;
+
+	int size_pos = 0;
+
+	int size_prev_pos = 0;
+
+	while ((size_pos = strText.find_first_of(strSeparator, size_pos)) != string::npos)
+
+	{
+
+		strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+
+		cout << strResult << " ";
+
+		size_prev_pos = ++size_pos;
+
+	}
+
+	if (size_prev_pos != strText.size())
+
+	{
+
+		strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+
+		cout << strResult << " ";
+
+	}
+
+	cout << endl;
 
 	return 0;
+
 }
 
+       */     
 
 
+//
+//class Solution {
+//public:
+//    int myAtoi(std::string s) {
+//        int size_pos = 0;
+//        int length = s.length();
+//        // 跳过前导空格
+//        while (size_pos < length && s[size_pos] == ' ') {
+//            ++size_pos;
+//        }
+//
+//        // 检查是否有正负号
+//        int sign = 1;
+//        if (size_pos < length && (s[size_pos] == '+' || s[size_pos] == '-')) {
+//            if (s[size_pos] == '-') {
+//                sign = -1;
+//            }
+//            ++size_pos;
+//        }
+//
+//        // 读入数字并转换为整数
+//        long result = 0;
+//        while (size_pos < length && isdigit(s[size_pos])) {
+//            result = result * 10 + (s[size_pos] - '0');
+//            // 检查是否溢出
+//            if (result * sign >= INT_MAX) {
+//                return INT_MAX;
+//            }
+//            if (result * sign <= INT_MIN) {
+//                return INT_MIN;
+//            }
+//            ++size_pos;
+//        }
+//
+//        return result * sign;
+//    }
+//};
+//
+//
 
 
-
-
-
-
-
-
-
-
-
+//
+//
+//class Solution {
+//public:
+//    int myAtoi(std::string str) {
+//        int size_pos = 0;
+//        int length = str.length();
+//        // 跳过前导空格
+//        while (size_pos < length && str[size_pos] == ' ') {
+//            ++size_pos;
+//        }
+//
+//        // 检查是否有正负号
+//        int sign = 1;
+//        if (size_pos < length && (str[size_pos] == '+' || str[size_pos] == '-')) {
+//            if (str[size_pos] == '-') {
+//                sign = -1;
+//            }
+//            ++size_pos;
+//        }
+//
+//        // 开始读取数字
+//        long result = 0;
+//        while (size_pos < length && isdigit(str[size_pos])) {
+//            result = result * 10 + (str[size_pos] - '0');
+//
+//            // 检查是否溢出
+//            if (sign * result > INT_MAX) {
+//                return INT_MAX;
+//            }
+//            if (sign * result < INT_MIN) {
+//                return INT_MIN;
+//            }
+//
+//            ++size_pos;
+//        }
+//
+//        return sign * result;
+//    }
+//};
 
 
 

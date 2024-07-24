dc06d5b915c0e7efb295689fb648b0ec89f8dd17
rollingtudou <13991770359@163.com>
2024-07-24 17:29:25 +0800

    第一次提交，测试文件

---
+++ String/string.cpp
@@ -0,0 +1,250 @@
+#include"string.h"
+
+namespace bit
+{
+	const size_t string::npos = -1;
+
+	void string::reserve(size_t n)
+	{
+		if (n > _capacity)
+		{
+			//cout << "reserve:" << n << endl;
+
+			char* tmp = new char[n + 1];
+			strcpy(tmp, _str);
+			delete[] _str;
+			_str = tmp;
+			_capacity = n;
+		}
+	}
+
+	void string::push_back(char ch)
+	{
+		if (_size == _capacity)
+		{
+			reserve(_capacity == 0 ? 4 : _capacity * 2);
+		}
+
+		_str[_size] = ch;
+		++_size;
+		_str[_size] = '\0';
+	}
+
+	string& string::operator+=(char ch)
+	{
+		push_back(ch);
+		return *this;
+	}
+
+	void string::append(const char* str)
+	{
+		size_t len = strlen(str);
+		if (_size + len > _capacity)
+		{
+			// 大于2倍，需要多少开多少，小于2倍按2倍扩
+			reserve(_size + len > 2 * _capacity ? _size + len : 2 * _capacity);
+		}
+
+		strcpy(_str + _size, str);
+		_size += len;
+	}
+
+	string& string::operator+=(const char* str)
+	{
+		append(str);
+		return *this;
+	}
+
+	void string::insert(size_t pos, char ch)
+	{
+		assert(pos <= _size);
+
+		if (_size == _capacity)
+		{
+			reserve(_capacity == 0 ? 4 : _capacity * 2);
+		}
+
+		// 挪动数据
+		size_t end = _size + 1;
+		while (end > pos)
+		{
+			_str[end] = _str[end - 1];
+			--end;
+		}
+
+		_str[pos] = ch;
+		++_size;
+	}
+
+	void string::insert(size_t pos, const char* s)
+	{
+		assert(pos <= _size);
+
+		size_t len = strlen(s);
+		if (_size + len > _capacity)
+		{
+			// 大于2倍，需要多少开多少，小于2倍按2倍扩
+			reserve(_size + len > 2 * _capacity ? _size + len : 2 * _capacity);
+		}
+
+		size_t end = _size + len;
+		while (end > pos + len - 1)
+		{
+			_str[end] = _str[end - len];
+			--end;
+		}
+
+		for (size_t i = 0; i < len; i++)
+		{
+			_str[pos + i] = s[i];
+		}
+
+		_size += len;
+	}
+
+	void string::erase(size_t pos, size_t len)
+	{
+		assert(pos < _size);
+
+		if (len >= _size - pos)
+		{
+			_str[pos] = '\0';
+			_size = pos;
+		}
+		else
+		{
+			for (size_t i = pos + len; i <= _size; i++)
+			{
+				_str[i - len] = _str[i];
+			}
+
+			_size -= len;
+		}
+	}
+
+	size_t string::find(char ch, size_t pos)
+	{
+		assert(pos < _size);
+
+		for (size_t i = pos; i < _size; i++)
+		{
+			if (_str[i] == ch)
+			{
+				return i;
+			}
+		}
+
+		return npos;
+	}
+
+	size_t string::find(const char* str, size_t pos)
+	{
+		assert(pos < _size);
+
+		const char* ptr = strstr(_str + pos, str);
+		if (ptr == nullptr)
+		{
+			return npos;
+		}
+		else
+		{
+			return ptr - _str;
+		}
+	}
+
+	string string::substr(size_t pos, size_t len)
+	{
+		assert(pos < _size);
+
+		// len大于剩余字符长度，更新一下len
+		if (len > _size - pos)
+		{
+			len = _size - pos;
+		}
+
+		string sub;
+		sub.reserve(len);
+		for (size_t i = 0; i < len; i++)
+		{
+			sub += _str[pos + i];
+		}
+
+		return sub;
+	}
+
+	bool operator<(const string& s1, const string& s2)
+	{
+		return strcmp(s1.c_str(), s2.c_str()) < 0;
+	}
+
+	bool operator<=(const string& s1, const string& s2)
+	{
+		return s1 < s2 || s1 == s2;
+	}
+
+	bool operator>(const string& s1, const string& s2)
+	{
+		return !(s1 <= s2);
+	}
+
+	bool operator>=(const string& s1, const string& s2)
+	{
+		return !(s1 < s2);
+	}
+
+	bool operator==(const string& s1, const string& s2)
+	{
+		return strcmp(s1.c_str(), s2.c_str()) == 0;
+	}
+
+	bool operator!=(const string& s1, const string& s2)
+	{
+		return !(s1 == s2);
+	}
+
+	ostream& operator<<(ostream& out, const string& s)
+	{
+		for (auto ch : s)
+		{
+			out << ch;
+		}
+
+		return out;
+	}
+
+	// 17：16继续
+	istream& operator>>(istream& in, string& s)
+	{
+		s.clear();
+
+		const int N = 256;
+		char buff[N];
+		int i = 0;
+
+		char ch;
+		//in >> ch;
+		ch = in.get();
+		while (ch != ' ' && ch != '\n')
+		{
+			buff[i++] = ch;
+			if (i == N - 1)
+			{
+				buff[i] = '\0';
+				s += buff;
+
+				i = 0;
+			}
+
+			//in >> ch;
+			ch = in.get();
+		}
+
+		if (i > 0)
+		{
+			buff[i] = '\0';
+			s += buff;
+		}
+
+		return in;
+	}
+}

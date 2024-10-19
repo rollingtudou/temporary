90aa85334ad617065d6787542627a77ee7f8c453
勤奋的土豆 <820806231@qq.com>
2024-10-19 22:39:00 +0800

    红黑树代码实现添加

--- 红黑树/RBTree.h
+++ 红黑树/RBTree.h
@@ -1,48 +1,60 @@
 #pragma once
 
+// 枚举定义红黑树节点的颜色
 enum Colour
 {
-	RED,
-	BLACK
+	RED,   // 红色节点
+	BLACK  // 黑色节点
 };
 
+// 模板结构体定义红黑树的节点
 template<class K, class V>
 struct RBTreeNode
 {
-	// 这里更新控制平衡也要加入parent指针
+	// 保存键值对
 	pair<K, V> _kv;
+
+	// 左右孩子节点指针，父节点指针
 	RBTreeNode<K, V>* _left;
 	RBTreeNode<K, V>* _right;
 	RBTreeNode<K, V>* _parent;
+
+	// 节点颜色（红色或黑色）
 	Colour _col;
 
+	// 构造函数，初始化键值对，并将子节点和父节点指针设置为空
 	RBTreeNode(const pair<K, V>& kv)
-		:_kv(kv)
+		: _kv(kv)
 		, _left(nullptr)
 		, _right(nullptr)
-		, _parent(nullptr)
+		, _parent(nullptr) 
 	{}
 };
 
+// 模板类定义红黑树
 template<class K, class V>
 class RBTree
 {
 	typedef RBTreeNode<K, V> Node;
+
 public:
+	// 插入键值对到红黑树中
 	bool Insert(const pair<K, V>& kv)
 	{
+		// 如果树为空，插入第一个节点，并将其颜色设置为黑色
 		if (_root == nullptr)
 		{
 			_root = new Node(kv);
-			_root->_col = BLACK;
-
+			_root->_col = BLACK;  // 根节点必须为黑色
 			return true;
 		}
 
+		// 遍历树找到插入位置
 		Node* parent = nullptr;
 		Node* cur = _root;
 		while (cur)
 		{
+			// 如果键大于当前节点，向右走；否则向左走
 			if (cur->_kv.first < kv.first)
 			{
 				parent = cur;
@@ -55,12 +67,16 @@ public:
 			}
 			else
 			{
+				// 键已存在，返回 false
 				return false;
 			}
 		}
 
+		// 创建新节点，并将其颜色设置为红色
 		cur = new Node(kv);
 		cur->_col = RED;
+
+		// 将新节点链接到其父节点
 		if (parent->_kv.first < kv.first)
 		{
 			parent->_right = cur;
@@ -69,99 +85,84 @@ public:
 		{
 			parent->_left = cur;
 		}
-		// 链接父亲
 		cur->_parent = parent;
 
-		// 父亲是红色，出现连续的红色节点，需要处理
+		// 如果父节点是红色，则修复红黑树属性
 		while (parent && parent->_col == RED)
 		{
 			Node* grandfather = parent->_parent;
-			if (parent == grandfather->_left)
+			if (parent == grandfather->_left) // parent 在左  uncle在右
 			{
-				//   g
-				// p   u
 				Node* uncle = grandfather->_right;
+
+				// 情况 1: 叔叔节点是红色，需要重新着色
 				if (uncle && uncle->_col == RED)
 				{
-					// 变色
 					parent->_col = uncle->_col = BLACK;
 					grandfather->_col = RED;
-
-					// 继续往上处理
-					cur = grandfather;
+					cur = grandfather;  // 继续向上修复
 					parent = cur->_parent;
 				}
 				else
 				{
+					// 情况 2: 叔叔节点是黑色或不存在，需要旋转
 					if (cur == parent->_left)
 					{
-						//     g
-						//   p    u
-						// c
-						RotateR(grandfather);
+						RotateR(grandfather);  // 右旋转
 						parent->_col = BLACK;
 						grandfather->_col = RED;
 					}
-					else
+					else // 单旋无法解决问题 
 					{
-						//      g
-						//   p    u
-						//     c
-						RotateL(parent);
-						RotateR(grandfather);
+						RotateL(parent);  // 先左旋父节点
+						RotateR(grandfather);  // 再右旋祖父节点
 						cur->_col = BLACK;
 						grandfather->_col = RED;
 					}
-
-					break;
+					// 叔叔节点是黑色或不存在时，在旋转处理后当前最上面的是黑色节点，所以已经满足红黑树要求，可以直接break
+					break;  
 				}
 			}
-			else
+			else // parent 在右  uncle在左
 			{
-				//   g
-				// u   p
 				Node* uncle = grandfather->_left;
-				// 叔叔存在且为红，-》变色即可
+
+				// 情况 1: 叔叔节点是红色，需要重新着色
 				if (uncle && uncle->_col == RED)
 				{
 					parent->_col = uncle->_col = BLACK;
 					grandfather->_col = RED;
-
-					// 继续往上处理
-					cur = grandfather;
+					cur = grandfather;  // 继续向上修复
 					parent = cur->_parent;
 				}
-				else // 叔叔不存在，或者存在且为黑
+				else
 				{
-					// 情况二：叔叔不存在或者存在且为黑
-					// 旋转+变色
-					//   g
-					// u   p
-					//       c
+					// 情况 2: 叔叔节点是黑色或不存在，需要旋转
 					if (cur == parent->_right)
 					{
-						RotateL(grandfather);
+						RotateL(grandfather);  // 左旋转
 						parent->_col = BLACK;
 						grandfather->_col = RED;
 					}
 					else
 					{
-						RotateR(parent);
-						RotateL(grandfather);
+						RotateR(parent);  // 先右旋父节点
+						RotateL(grandfather);  // 再左旋祖父节点
 						cur->_col = BLACK;
 						grandfather->_col = RED;
 					}
-
+					// 叔叔节点是黑色或不存在时，在旋转处理后当前最上面的是黑色节点，所以已经满足红黑树要求，可以直接break
 					break;
 				}
 			}
 		}
 
+		// 确保根节点始终为黑色
 		_root->_col = BLACK;
-
 		return true;
 	}
 
+	// 右旋转以修复红黑树平衡
 	void RotateR(Node* parent)
 	{
 		Node* subL = parent->_left;
@@ -172,10 +173,10 @@ public:
 			subLR->_parent = parent;
 
 		Node* pParent = parent->_parent;
-
 		subL->_right = parent;
 		parent->_parent = subL;
 
+		// 处理根节点的情况
 		if (parent == _root)
 		{
 			_root = subL;
@@ -191,15 +192,16 @@ public:
 			{
 				pParent->_right = subL;
 			}
-
 			subL->_parent = pParent;
 		}
 	}
 
+	// 左旋转以修复红黑树平衡
 	void RotateL(Node* parent)
 	{
 		Node* subR = parent->_right;
 		Node* subRL = subR->_left;
+
 		parent->_right = subRL;
 		if (subRL)
 			subRL->_parent = parent;
@@ -207,6 +209,8 @@ public:
 		Node* parentParent = parent->_parent;
 		subR->_left = parent;
 		parent->_parent = subR;
+
+		// 处理根节点的情况
 		if (parentParent == nullptr)
 		{
 			_root = subR;
@@ -226,22 +230,26 @@ public:
 		}
 	}
 
+	// 中序遍历树（左-根-右）
 	void InOrder()
 	{
 		_InOrder(_root);
 		cout << endl;
 	}
 
+	// 计算树的高度
 	int Height()
 	{
 		return _Height(_root);
 	}
 
+	// 计算树的大小（节点数）
 	int Size()
 	{
 		return _Size(_root);
 	}
 
+	// 查找具有特定键的节点
 	Node* Find(const K& key)
 	{
 		Node* cur = _root;
@@ -264,15 +272,17 @@ public:
 		return nullptr;
 	}
 
+	// 检查红黑树是否平衡
 	bool IsBalance()
 	{
 		if (_root == nullptr)
 			return true;
 
+		// 根节点必须为黑色
 		if (_root->_col == RED)
 			return false;
 
-		// 参考值
+		// 比较从根到叶子的所有路径中的黑节点数量  作为黑节点的基准值传入Check进行判断红黑树是否正确
 		int refNum = 0;
 		Node* cur = _root;
 		while (cur)
@@ -284,41 +294,45 @@ public:
 			cur = cur->_left;
 		}
 
+		// 递归检查所有路径是否具有相同的黑节点数量
 		return Check(_root, 0, refNum);
 	}
 
 private:
 
+	// 递归检查树是否满足红黑树的属性
 	bool Check(Node* root, int blackNum, const int refNum)
 	{
 		if (root == nullptr)
 		{
-			// 前序遍历走到空时，意味着一条路径走完了
-			//cout << blackNum << endl;
+			// 如果到达叶子节点，检查黑色节点数量
 			if (refNum != blackNum)
 			{
-				cout << "存在黑色结点的数量不相等的路径" << endl;
+				cout << "路径中的黑色节点数量不相等" << endl;
 				return false;
 			}
 			return true;
 		}
 
-		// 检查孩子不太方便，因为孩子有两个，且不一定存在，反过来检查父亲就方便多了
+		// 确保没有连续的红色节点
 		if (root->_col == RED && root->_parent->_col == RED)
 		{
-			cout << root->_kv.first << "存在连续的红色结点" << endl;
+			cout << root->_kv.first << "存在连续的红色节点" << endl;
 			return false;
 		}
 
+		// 如果当前节点是黑色，增加黑色节点数量
 		if (root->_col == BLACK)
 		{
 			blackNum++;
 		}
 
+		// 检查左右子树是否满足红黑树性质
 		return Check(root->_left, blackNum, refNum)
 			&& Check(root->_right, blackNum, refNum);
 	}
 
+	// 中序遍历的辅助函数
 	void _InOrder(Node* root)
 	{
 		if (root == nullptr)
@@ -331,23 +345,28 @@ private:
 		_InOrder(root->_right);
 	}
 
+	// 递归计算树的高度
 	int _Height(Node* root)
 	{
 		if (root == nullptr)
 			return 0;
 		int leftHeight = _Height(root->_left);
 		int rightHeight = _Height(root->_right);
+		// 返回较大的子树高度+1
 		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
 	}
 
+	// 递归计算树的大小
 	int _Size(Node* root)
 	{
 		if (root == nullptr)
 			return 0;
 
+		// 左子树大小 + 右子树大小 + 当前节点
 		return _Size(root->_left) + _Size(root->_right) + 1;
 	}
 
 private:
+	// 红黑树的根节点
 	Node* _root = nullptr;
 };
